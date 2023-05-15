f6f99687fe953cbb4a9baa91a37307b46b0918a3
skjunseo0103 <hazzisss5249@gmail.com>
2023-05-16 04:01:34 +0900

    elastic_search test

--- jskim/Elasticsearch_GPT3/es_gpt_test.py
+++ jskim/Elasticsearch_GPT3/es_gpt_test.py
@@ -1,35 +1,66 @@
 import openai
 from elasticsearch import Elasticsearch
 
-# OpenAI API key
-openai.api_key = "YOUR_API_KEY_HERE"
-
 # Elasticsearch host
-es = Elasticsearch([{"host": "localhost", "port": 9200}])
 
-def search_and_generate_text(query):
-    # Elasticsearch query
-    es_query = {
-        "query": {
-            "match": {
-                "content": query
+class es_gpt():
+    ## 초기화
+    def __init__(self,api_key):
+        self.api_key = api_key
+        self.query = None
+        self.result = None
+        openai.api_key = self.api_key 
+        es_host = "http://localhost:9200"    
+        self.es = Elasticsearch([es_host]) 
+    ## 데이터를 넣는 코드
+    # 인덱스 생성
+    def make_index(self, doc: dict): ## 딕셔너리 형태로 넣어주자.
+        """
+        input type:
+        doc_dict = {'title': 'Example Document', 'content': 'This is an example document.'}
+        make_index(doc_dict)
+        """
+        try:
+            # 인덱스 생성
+            if not self.es.indices.exists(index='my_index'):
+                self.es.indices.create(index='my_index')
+            # 문서 추가
+            # doc = {'title': 'Example Document', 'content': 'This is an example document.'}
+            self.es.index(index='my_index', body=doc)
+            print("인덱스 생성 완료")
+        except Exception as e:
+            # 예외 처리 코드
+            print("An error occurred:", str(e))
+
+
+    ## 데이터를 찾는 코드
+    def search_es(self, query): ## 검색할 데이터를 넣는다.
+        # Elasticsearch query
+        self.query = query
+        es_query = {
+            "query": {
+                "match": {
+                    "content": self.query
+                }
             }
         }
-    }
+        # 'my_index' 인덱스의 'content' 필드에서 'example'를 검색하는 쿼리 실행
+        results = self.es.search(index="my_index", body=es_query)
+        self.result = results['hits']['hits']
+        # print(self.result['hits']['hits'])
+        return self.result
 
-    # Search the Elasticsearch index
-    results = es.search(index="my_index", body=es_query)
+    ## 찾은 데이터를 chat gpt로 수정하는 코드
+    def search_and_generate_text(self, query): # 찾는 부분
 
-    # Extract the text from the Elasticsearch results
-    text = "\n".join(hit["_source"]["content"] for hit in results["hits"]["hits"])
-
-    # Generate text using GPT-3
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=text,
-        max_tokens=1024
-    )
-
-    # Return the generated text
-    return response.choices[0].text
+        # Extract the text from the Elasticsearch results
+        text = "\n".join(hit["_source"]["content"] for hit in self.result)
+        # Generate text using GPT-3
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=text,
+            max_tokens=1024
+        )
+        # Return the generated text
+        return response.choices[0].text
 
--- jskim/Elasticsearch_GPT3/setup.py
+++ jskim/Elasticsearch_GPT3/setup.py
@@ -1,25 +1,30 @@
-# FIXME
-PROJECT_NAME=??
+from setuptools import find_packages, setup
 
-# Specify the names of all executables to make.
-PROG=update install format lint precommit check
-.PHONY: ${PROG}
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
-update:
-	pip install --upgrade pip wheel
-	pip install --upgrade -r requirements.txt
-
-install:
-	pip install --upgrade pip wheel
-	pip install -r requirements.txt
-
-format:
-	black .
-	isort .
-
-lint:
-	pytest ${PROJECT_NAME} --flake8 ## 코드 컨벤션 감지기
-
-precommit: format
-
-check: format lint
+setup(
+    name='psat_coop',
+    version='0.0.1',
+    ## requierement 설치
+    packages=find_packages(),
+    ## 명령어로 사용하게끔
+    ## terminal 명령어 = 폴더.파일:함수
+    entry_points={
+        "console_scripts": [
+            "ship-cli = shipcommand.cli:cli"
+        ]
+    },
+    ## 이하의 폴더에서 경로에서 import
+    py_modules=["src"],
+    ## 이하 안 중요
+    url='https://github.com/psat-foodplanet/interactive-search',
+    license='MIT',
+    author='PSAT',
+    author_email='skjunseo0103@naver.com',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+    # install_requires=required,
+    description='Interactive Search Project'
+)
