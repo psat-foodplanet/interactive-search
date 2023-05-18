a13b3ef02dd2c7633134388f5554fdb5e1bd71bc
skjunseo0103 <hazzisss5249@gmail.com>
2023-05-19 01:34:35 +0900

    05-19

--- jskim/Elasticsearch_GPT3/es_gpt_test.py
+++ jskim/Elasticsearch_GPT3/es_gpt_test.py
@@ -9,23 +9,74 @@ class es_gpt():
         self.api_key = api_key
         self.query = None
         self.result = None
-        openai.api_key = self.api_key 
-        es_host = "http://localhost:9200"    
-        self.es = Elasticsearch([es_host]) 
+        self.token = "standard"
+        openai.api_key = self.api_key
+        es_host = "http://localhost:9200"
+        self.es = Elasticsearch([es_host])
+
     ## 데이터를 넣는 코드
     # 인덱스 생성
-    def make_index(self, doc: dict): ## 딕셔너리 형태로 넣어주자.
+    # def make_index(self, doc: dict): ## 딕셔너리 형태로 넣어주자.
+    #     """
+    #     input type:
+    #     doc_dict = {'title': 'Example Document', 'content': 'This is an example document.'}
+    #     make_index(doc_dict)
+    #     """
+    #     try:
+    #         # 인덱스 생성
+    #         if not self.es.indices.exists(index='my_index'):
+    #             self.es.indices.create(index='my_index')
+    #         # 문서 추가
+    #         # doc = {'title': 'Example Document', 'content': 'This is an example document.'}
+
+    #         # 필드 매핑 설정
+    #         mapping = {
+    #             "properties": {
+    #                 "content": {
+    #                     "type": "text",
+    #                     "analyzer": "keyword"
+    #                 }
+    #             }
+    #         }
+
+    #         self.es.indices.put_mapping(index='my_index', body=mapping)
+    #         self.es.index(index='my_index', body=doc)
+    #         print("인덱스 생성 완료")
+    #     except Exception as e:
+    #         # 예외 처리 코드
+    #         print("An error occurred:", str(e))
+
+    def make_index(self, doc: dict, Token: str):
         """
         input type:
         doc_dict = {'title': 'Example Document', 'content': 'This is an example document.'}
         make_index(doc_dict)
         """
+        self.token = Token
         try:
             # 인덱스 생성
             if not self.es.indices.exists(index='my_index'):
                 self.es.indices.create(index='my_index')
+
+                # 기존 매핑 확인
+                existing_mapping = self.es.indices.get_mapping(index='my_index')
+
+                if 'content' in existing_mapping['my_index']['mappings']:
+                    existing_analyzer = existing_mapping['my_index']['mappings']['content']['analyzer']
+
+                    # 기존 매핑과 토크나이저가 다른 경우에만 매핑 업데이트
+                    if existing_analyzer != self.token:
+                        mapping = {
+                            "properties": {
+                                "content": {
+                                    "type": "text",
+                                    "analyzer": self.token
+                                }
+                            }
+                        }
+                        self.es.indices.put_mapping(index='my_index', body=mapping)
+
             # 문서 추가
-            # doc = {'title': 'Example Document', 'content': 'This is an example document.'}
             self.es.index(index='my_index', body=doc)
             print("인덱스 생성 완료")
         except Exception as e:
@@ -63,4 +114,3 @@ class es_gpt():
         )
         # Return the generated text
         return response.choices[0].text
-
